ae6f4cd11434ea1b572fb1d27f9eca13b8205bde
Michel Rodrigues Andrade <michel.andrade@fisica.ufc.br>
2023-08-09 14:34:10 -0300

    Introduzindo o pacote json temporariamente

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 import xmltodict # faz a leitura xml e transforma para dicionário
 import os  # manusear arquivos
 import pandas as pd
+import json
 
 def pegar_infor(arquivo):
     print(f"Pegou as informações {arquivo}")
     with open(f'nfs/{arquivo}', "rb") as arquivo_xml:
         dic_arquivo = xmltodict.parse(arquivo_xml)
-        print(dic_arquivo)
+        print(json.dumps(dic_arquivo, index=4)) # melhorar a visualização dos arquivos, onde index dará 4 espaços entre os prints
 
 
 lista_arquivos = os.listdir("nfs")
