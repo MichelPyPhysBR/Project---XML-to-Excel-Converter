ca8696899991213749cb2d09a1da4bc5d8383e5b
Michel Rodrigues Andrade <michel.andrade@fisica.ufc.br>
2023-08-09 14:58:31 -0300

    retirando os arquivos temporários.

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import xmltodict # faz a leitura xml e transforma para dicionário
+import xmltodict  # faz a leitura xml e transforma para dicionário
 import os  # manusear arquivos
 import pandas as pd
-import json
 
 
 def pegar_infor(arquivo):
@@ -9,23 +8,19 @@ def pegar_infor(arquivo):
     with open(f'nfs/{arquivo}', "rb") as arquivo_xml:
         dic_arquivo = xmltodict.parse(arquivo_xml)
 
-        try:
-            if "NFe" in dic_arquivo:
-                infor_nf = dic_arquivo["NFe"]["infNFe"]
-            else:
-                infor_nf = dic_arquivo["nfeProc"]["NFe"]["infNFe"]
-            numero_nota = infor_nf["@Id"]
-            empresa_emissora = infor_nf["emit"]["xNome"]
-            nome_cliente = infor_nf["dest"]["xNome"]
-            endereco = infor_nf["dest"]["enderDest"]
-            if "vol" in dic_arquivo:
-                peso = infor_nf["transp"]["vol"]["pesoB"]
-            else:
-                peso = "Não informado!"
-            print(numero_nota, empresa_emissora, nome_cliente, endereco, peso, sep="\n")
-        except Exception as e:
-            print(e)
-            print(json.dumps(dic_arquivo, indent=4))
+        if "NFe" in dic_arquivo:
+            infor_nf = dic_arquivo["NFe"]["infNFe"]
+        else:
+            infor_nf = dic_arquivo["nfeProc"]["NFe"]["infNFe"]
+        numero_nota = infor_nf["@Id"]
+        empresa_emissora = infor_nf["emit"]["xNome"]
+        nome_cliente = infor_nf["dest"]["xNome"]
+        endereco = infor_nf["dest"]["enderDest"]
+        if "vol" in dic_arquivo:
+            peso = infor_nf["transp"]["vol"]["pesoB"]
+        else:
+            peso = "Não informado!"
+        print(numero_nota, empresa_emissora, nome_cliente, endereco, peso, sep="\n")
 
 
 lista_arquivos = os.listdir("nfs")
